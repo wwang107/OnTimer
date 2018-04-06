f341761c78ab4d27d51cee87a48c8fa68c41c0af
Wei <wwang107@gmail.com>
2018-04-06 20:48:23 +0800

    pop-up can now close by closed button

--- js/main.js
+++ js/main.js
@@ -1,5 +1,6 @@
 var modal = document.querySelector('.modal');
 var btns = document.querySelectorAll('.feature-btn');
+var closeBtns = document.querySelectorAll('.close');
 var ps = document.querySelectorAll('.feature-detail');
 // Add buttons behavior: pop-up feature description and show the specific feature detail
 for (var i = 0; i < btns.length; i++){ 
@@ -9,10 +10,15 @@ for (var i = 0; i < btns.length; i++){
         p.style.display = 'block'; // show clicked feature detail
     });
 }
-//
-// closeBtn.onclick = function () {
-//     modal.style.display = "none";
-// }
+
+for (var i = 0; i < closeBtns.length; i++) {
+    closeBtns[i].addEventListener('click',function(){
+        modal.style.display = "none";
+        for (var i = 0; i < ps.length; i++) {
+            ps[i].style.display = 'none'; // set the clicked feature display attribute back to "None" once the pop-up closed
+        }
+    });
+}
 
 window.onclick = function (event) {
     if (event.target == modal) {
