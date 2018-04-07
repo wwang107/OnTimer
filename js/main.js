2058c7dd860c3122ef1bfb4412c43b43cb995917
Wei <wwang107@gmail.com>
2018-04-07 11:21:35 +0800

    pop-up text alighment

--- js/main.js
+++ js/main.js
@@ -1,35 +1,39 @@
 
+// modal funnality va
 var body = document.getElementsByTagName("BODY")[0]
 var modal = document.querySelector('.modal');
-var btns = document.querySelectorAll('.feature-btn');
+var featureBtns = document.querySelectorAll('.feature-btn');
 var closeBtns = document.querySelectorAll('.close');
-var ps = document.querySelectorAll('.feature-detail');
-// Add buttons behavior: pop-up feature description and show the specific feature detail
-for (var i = 0; i < btns.length; i++){ 
-    btns[i].addEventListener('click', function(){
+var featureDetailS = document.querySelectorAll('.feature-detail');
+
+// Open pop-up buttons behavior: pop-up feature description and show the specific feature detail
+for (var i = 0; i < featureBtns.length; i++){ 
+    featureBtns[i].addEventListener('click', function(){
         body.classList.add('scroll-lock');
         modal.style.display = "block";
-        p = document.querySelectorAll('#'+this.id)[1];
-        p.style.display = 'block'; // show clicked feature detail
+        featureDetail = document.querySelectorAll('#'+this.id)[1]; //use the id of the button to get the correspond modal content
+        featureDetail.style.display = 'block'; // show clicked feature detail
     });
 }
 
+// Closed pop-up buttons behaviors
 for (var i = 0; i < closeBtns.length; i++) {
     closeBtns[i].addEventListener('click',function(){
         body.classList.remove('scroll-lock');
         modal.style.display = "none";
-        for (var i = 0; i < ps.length; i++) {
-            ps[i].style.display = 'none'; // set the clicked feature display attribute back to "None" once the pop-up closed
+        for (var i = 0; i < featureDetailS.length; i++) {
+            featureDetailS[i].style.display = 'none'; // set the clicked feature display attribute back to "None" once the pop-up closed
         }
     });
 }
 
+// Closed pop-up buttons behaviors: when clicking outside the pop-up content, the pop-up will close
 window.onclick = function (event) {
     if (event.target == modal) {
         body.classList.remove('scroll-lock');
         modal.style.display = "none";
-        for (var i=0; i<ps.length; i++){
-            ps[i].style.display = 'none'; // set the clicked feature display attribute back to "None" once the pop-up closed
+        for (var i = 0; i < featureDetailS.length; i++){
+            featureDetailS[i].style.display = 'none'; // set the clicked feature display attribute back to "None" once the pop-up closed
         }
     }
 } 
